e44e1866db7aa71aca6573b6651f2aff8468e773
Raseswari Sarangi <83106715+RaseswariSarangi@users.noreply.github.com>
2022-01-29 08:39:16 +0530

    Add files via upload

---
+++ tic tac toe/tictactoe.py
@@ -0,0 +1,159 @@
+from tkinter import *                                                # imports every modules
+import tkinter.messagebox                                            # for showing the message tie/win
+
+root = Tk()                                                           # helps in using tkinter methods
+root.iconbitmap("tic tac toe.ico")                                   # only takes .ico image
+root.title("Tic Tac Toe")                                            # for title of game
+root.resizable(width=False, height=False)                             # doesn't allow user to change
+
+# global variables
+click = True
+count = 0                                                            # keeps track of the moves(if moves=9-tie game)
+
+# text variables-for buttons
+btn1 = StringVar()
+btn2 = StringVar()
+btn3 = StringVar()
+btn4 = StringVar()
+btn5 = StringVar()
+btn6 = StringVar()
+btn7 = StringVar()
+btn8 = StringVar()
+btn9 = StringVar()
+
+# using photos
+xPhoto = PhotoImage(file='X.png')
+oPhoto = PhotoImage(file='O.png')
+
+# func for game
+# Check the game
+def play():
+     button1 = Button(root, height=9, width=19, borderwidth=0.5, relief='raised', background='#ccccff', textvariable=btn1, command=lambda:press(1,0,0))
+     button1.grid(row=0,column=0)
+     button2 = Button(root, height=9, width=19, borderwidth=0.5, relief='raised', background='#ccccff', textvariable=btn2, command=lambda:press(2,0,1))
+     button2.grid(row=0,column=1)
+     button3 = Button(root, height=9, width=19, borderwidth=0.5, relief='raised', background='#ccccff', textvariable=btn3, command=lambda: press(3,0,2))
+     button3.grid(row=0,column=2)
+     button4 = Button(root, height=9, width=19, borderwidth=0.5, relief='raised', background='#b3b3ff', textvariable=btn4, command=lambda: press(4,1,0))
+     button4.grid(row=1,column=0)
+     button5 = Button(root, height=9, width=19, borderwidth=0.5, relief='raised', background='#b3b3ff', textvariable=btn5, command=lambda: press(5,1,1))
+     button5.grid(row=1,column=1)
+     button6 = Button(root, height=9, width=19, borderwidth=0.5, relief='raised', background='#b3b3ff', textvariable=btn6, command=lambda: press(6,1,2))
+     button6.grid(row=1,column=2)
+     button7 = Button(root, height=9, width=19, borderwidth=0.5, relief='raised', background='#9999ff', textvariable=btn7, command=lambda: press(7,2,0))
+     button7.grid(row=2,column=0)
+     button8 = Button(root, height=9, width=19, borderwidth=0.5, relief='raised', background='#9999ff', textvariable=btn8, command=lambda: press(8,2,1))
+     button8.grid(row=2,column=1)
+     button9 = Button(root, height=9, width=19, borderwidth=0.5, relief='raised', background='#9999ff', textvariable=btn9, command=lambda: press(9,2,2))
+     button9.grid(row=2,column=2)
+#play()
+
+
+# check the button pressed
+# r-row, c-column
+def press(num, r, c):
+     global click,count
+     if click == True:
+         labelPhoto = Label(root,image=xPhoto)
+         labelPhoto.grid(row=r,column=c)
+         if num==1:
+             btn1.set('X')
+         elif num==2:
+             btn2.set('X')
+         elif num==3:
+             btn3.set('X')
+         elif num==4:
+             btn4.set('X')
+         elif num==5:
+             btn5.set('X')
+         elif num==6:
+             btn6.set('X')
+         elif num==7:
+             btn7.set('X')
+         elif num==8:
+             btn8.set('X')
+         else:
+             btn9.set('X')
+         count+=1
+         click=False
+         checkWin()
+     else:
+         labelPhoto = Label(root,image=oPhoto)
+         labelPhoto.grid(row=r,column=c)
+         if num==1:
+             btn1.set('O')
+         elif num==2:
+             btn2.set('O')
+         elif num==3:
+             btn3.set('O')
+         elif num==4:
+             btn4.set('O')
+         elif num==5:
+             btn5.set('O')
+         elif num==6:
+             btn6.set('O')
+         elif num==7:
+             btn7.set('O')
+         elif num==8:
+             btn8.set('O')
+         else:
+             btn9.set('O')
+         count+=1
+         click=True
+         checkWin()
+#play()
+
+
+# checks who wins and possibilities of win
+#possiblities:(1,2,3),(4,5,6),(7,8,9),(1,4,7),(2,5,8),(3,6,9),(1,5,9),(3,5,7)
+def checkWin():
+     global count,click
+     if(btn1.get()=='X' and btn2.get()=='X' and btn3.get()=='X' or
+        btn4.get()=='X' and btn5.get()=='X' and btn6.get()=='X' or
+        btn7.get()=='X' and btn8.get()=='X' and btn9.get()=='X' or
+        btn1.get()=='X' and btn4.get()=='X' and btn7.get()=='X' or
+        btn2.get()=='X' and btn5.get()=='X' and btn8.get()=='X' or
+        btn3.get()=='X' and btn6.get()=='X' and btn9.get()=='X' or
+        btn1.get()=='X' and btn5.get()=='X' and btn9.get()=='X' or
+        btn3.get()=='X' and btn5.get()=='X' and btn7.get()=='X'):
+        tkinter.messagebox.showinfo(title="Tic Tac Toe",message='X wins!')
+        click = True
+        count=0
+        clear()
+        play()
+     elif(btn1.get()=='O' and btn2.get()=='O' and btn3.get()=='O' or
+        btn4.get()=='O' and btn5.get()=='O' and btn6.get()=='O' or
+        btn7.get()=='O' and btn8.get()=='O' and btn9.get()=='O' or
+        btn1.get()=='O' and btn4.get()=='O' and btn7.get()=='O' or
+        btn2.get()=='O' and btn5.get()=='O' and btn8.get()=='O' or
+        btn3.get()=='O' and btn6.get()=='O' and btn9.get()=='O' or
+        btn1.get()=='O' and btn5.get()=='O' and btn9.get()=='O' or
+        btn3.get()=='O' and btn5.get()=='O' and btn7.get()=='O'):
+        tkinter.messagebox.showinfo(title="Tic Tac Toe",message='O wins!')
+        count=0
+        clear()
+        play()
+     elif(count==9):
+         tkinter.messagebox.showinfo(title="Tic Tac Toe",message='Tie Game!')
+         click=True
+         count=0
+         clear()
+         play()
+
+
+
+# clears text variables
+def clear():
+    btn1.set('')
+    btn2.set('')
+    btn3.set('')
+    btn4.set('')
+    btn5.set('')
+    btn6.set('')
+    btn7.set('')
+    btn8.set('')
+    btn9.set('')
+play()
+
+root.mainloop()                                                       # working with buttons in loop
+
